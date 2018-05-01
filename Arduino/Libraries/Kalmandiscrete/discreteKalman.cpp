70b500558733e466b9ccae106a5c57b5a1f88aa0
Matthias Rowold <matthiasrowold@googlemail.com>
2018-05-01 09:45:40 +0200

    changes

--- Arduino/Libraries/Kalmandiscrete/discreteKalman.cpp
+++ Arduino/Libraries/Kalmandiscrete/discreteKalman.cpp
@@ -2,7 +2,7 @@
 #include <discreteKalman.h>
 
 discreteKalman::discreteKalman() {
-	// Radius of circle around tags
+	// Radius of detection area around tags
 	Radius = 30;
 	
 	// COvariance for area around tags
@@ -67,16 +67,16 @@ discreteKalman::discreteKalman() {
 }
 
 void discreteKalman::orientationRobot(Pose SRT_Pose, float* tag_prev, float* tag_det){
-	double tag_dis[2];
-	double pose_dis[2];
+	double tag_dis[2]; //destances between tags (x and y)
+	double pose_dis[2]; // distances between current estimated pose and previous tag
 	// centre_dists between tags and poses at tags
 	for (int i=0;i<2;i++){
 		tag_dis[i] = tag_det[i] - tag_prev[i];
 		pose_dis[i] = SRT_Pose.globalPose[i] - tag_prev[i];
 	}
 	
-	// heading can be estimated after detectin two RFID tags
-	yaw_est = SRT_Pose.globalPose[2] + atan2(tag_dis[1], tag_dis[0]) - atan2();
+	// heading can be estimated after detecting two RFID tags
+	yaw_est = SRT_Pose.globalPose[2] + atan2(tag_dis[1], tag_dis[0]) - atan2(pose_dis[1], pose_dis[0]);
 }
 
 void discreteKalman::newTagdetected(float x, float y){
@@ -89,7 +89,7 @@ void discreteKalman::newTagdetected(float x, float y){
 	tag_det[1] = y;
 }
 
-void discreteKalman::CalcNewState(Pose SRT_Pose){
+void discreteKalman::calcNewState(Pose SRT_Pose){
 	// If the robot is estimated to be inside the tag zone, 
 	// an update is not neccessary
 	if(checkRobotinZone(SRT_Pose)){
@@ -99,11 +99,11 @@ void discreteKalman::CalcNewState(Pose SRT_Pose){
 	}
 	// if the robot is outside the tag zone--> update
 	else{
-		orientationRobot();
+		//orientationRobot();
 		float correction[2];
-		calcKalmanGain();
+		//calcKalmanGain();
 		// Sensor prediction is skipped --> global pose is the predicted sensor values
-		Matrix.Multiply((float*) Kalman_Gain, (float*) correction, 3, 2, 1, (float*) correction)
+		Matrix.Multiply((float*) Kalman_Gain, (float*) correction, 3, 2, 1, (float*) correction);
 		new_State[0] = SRT_Pose.globalPose[0] + correction[0];
 		new_State[1] = SRT_Pose.globalPose[1] + correction[1];
 		new_State[2] = yaw_est;
@@ -115,26 +115,26 @@ bool discreteKalman::checkRobotinZone(Pose SRT_Pose){
 	for (int i=0;i<2;i++){
 		centre_dist[i] = tag_det[i] - SRT_Pose.globalPose[i];
 	}
-	if(sqrt(sq(centre_dist[0])+sq(centre_dist[1])<Radius){
+	if(sqrt(sq(centre_dist[0])+sq(centre_dist[1])<Radius)){
 		return true;
 	}
 	else return false;
 }
 
-void KalmanFus::calcKalmanGain() {
+void discreteKalman::calcKalmanGain() {
 	float Product[2][2];
 	float Error_Sensor_Product[3][2];
 	float Sum[2][2]; // This is also the inverted Sum matrix!
-	Matrix.Multiply((float*) P, (float*) H_tran, 3, 3, 2, (float*) Error_Sensor_Product);
+	Matrix.Multiply((float*) P, (float*) H_trans, 3, 3, 2, (float*) Error_Sensor_Product);
 	Matrix.Multiply((float*) H, (float*) Error_Sensor_Product, 2, 3, 2,  (float*) Product);
-	Matrix.Add = ((float*) Product, (float*) R, 2, 2, (float*) Sum );
+	Matrix.Add((float*) Product, (float*) R, 2, 2, (float*) Sum );
 	Matrix.Invert((float*) Sum, 2);
-	Matrix.Multiply((foat*) Error_Sensor_Product, (float*) Sum, 3, 2, 2, (float*) Kalman_Gain);
+	Matrix.Multiply((float*) Error_Sensor_Product, (float*) Sum, 3, 2, 2, (float*) Kalman_Gain);
 	// Transpose Galman Gain matrix
 	Matrix.Transpose((float*) Kalman_Gain, 3, 3, (float*) Kalman_Gain_trans);
 }
 
-void updateErrorModel(){
+void discreteKalman::updateErrorModel(){
 	float KQ_Product[3][2];
 	float KQK_Product[3][3];
 	float KH_Product[3][3];
@@ -142,10 +142,9 @@ void updateErrorModel(){
 	float Diff_P_Product[3][3];
 	float P_new[3][3];
 	Matrix.Multiply((float*) Kalman_Gain_trans, (float*) Q, 3, 2, 2, (float*) KQ_Product);
-	Matrix.Multiply((float*) Kalman_Q_Product, (float*) Kalman_Gain_trans, 3, 2, 3, (float*) KQK_Product);
+	Matrix.Multiply((float*) KQ_Product, (float*) Kalman_Gain_trans, 3, 2, 3, (float*) KQK_Product);
 	Matrix.Multiply((float*) Kalman_Gain, (float*) H, 3, 2, 3, (float*) KH_Product);
 	Matrix.Subtract((float*) Eye, (float*) KH_Product, 3, 3, (float*) I_KH_Diff);
 	Matrix.Multiply((float*) I_KH_Diff, (float*) P, 3, 3, 3, (float*) Diff_P_Product);
-	Matrix.Add = ((float*) Diff_P_Product, (float*) KQK_Produc, 3, 3, (float*)P_new );
-
+	Matrix.Add((float*) Diff_P_Product, (float*) KQK_Product, 3, 3, (float*)P_new );
 }
